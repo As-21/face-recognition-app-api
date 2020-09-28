ec838ca6001dab65340f9de3c5b565823f27eae9
As-21 <as_21@live.com>
2020-09-28 20:40:39 +0300

    lets try again with connection error 9

--- controllers/register.js
+++ controllers/register.js
@@ -2,7 +2,7 @@
 const handleRegister = (db, bcrypt) => (req, res) => {
     const { email, name, password } = req.body;
     if (!email || !name || !password) {
-        return res.status(400).json('Incorrect form submission');
+        return res.status(400).json('incorrect form submission');
     }
     const hash = bcrypt.hashSync(password);
     db.transaction(trx => {
@@ -16,18 +16,19 @@ const handleRegister = (db, bcrypt) => (req, res) => {
                 return trx('users')
                     .returning('*')
                     .insert({
-                        email: loginEmail[0],
                         name: name,
+                        email: loginEmail[0],
                         joined: new Date()
-                    }).then(user => {
+                    })
+                    .then(user => {
                         res.json(user[0]);
                     })
             })
             .then(trx.commit)
             .catch(trx.rollback)
     })
-        .catch(err => res.status(400).json('Unable to register'));
-};
+        .catch(err => res.status(400).json(err))
+}
 
 module.exports = {
     handleRegister: handleRegister
