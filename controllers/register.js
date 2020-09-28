ca2fc5c23fef175c8def15c8a8123f3abdd57dab
As-21 <as_21@live.com>
2020-09-28 20:23:16 +0300

    lets try again with connection error 5

--- controllers/register.js
+++ controllers/register.js
@@ -1,6 +1,6 @@
 
 const handleRegister = (db, bcrypt) => (req, res) => {
-    const { email, name, password } = req.body;
+    const { email, password, name } = req.body;
     if (!email || !name || !password) {
         return res.status(400).json('Incorrect form submission');
     }
